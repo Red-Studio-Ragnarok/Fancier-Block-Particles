df5c911e63977be3c4f2aeef737523833e4f0982
Desoroxxx <desoroxxx@gmail.com>
2022-08-31 02:48:51 +0200

    Update Default Bindings

--- src/main/java/com/TominoCZ/FBP/keys/FBPKeyBindings.java
+++ src/main/java/com/TominoCZ/FBP/keys/FBPKeyBindings.java
@@ -1,17 +1,14 @@
 package com.TominoCZ.FBP.keys;
 
-import org.lwjgl.input.Keyboard;
-
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import org.lwjgl.input.Keyboard;
 
-public class FBPKeyBindings
-{
+public class FBPKeyBindings {
 	public static KeyBinding FBPMenu, FBPFreeze, FBPToggle, FBPSweep, FBPFastAdd;
 
-	public static void init()
-	{
-		FBPMenu = new KeyBinding("Open Menu", Keyboard.KEY_NONE, "Fancy Block Particles");
+	public static void init() {
+		FBPMenu = new KeyBinding("Open Menu", Keyboard.KEY_P, "Fancy Block Particles");
 		FBPFreeze = new KeyBinding("Toggle Freeze Effect", Keyboard.KEY_NONE, "Fancy Block Particles");
 		FBPToggle = new KeyBinding("Enable/Disable", Keyboard.KEY_NONE, "Fancy Block Particles");
 		FBPSweep = new KeyBinding("Kill Particles", Keyboard.KEY_NONE, "Fancy Block Particles");
