fac1391390ca095294b74ea5934e17463572472b
Desoroxxx <desoroxxx@gmail.com>
2022-10-03 14:10:58 +0200

    Revert "Revert "Fixed Something In FBPEventHandler, Code Cleanup & Minor Optimization""
    This reverts commit acb5fecadf01e33b02b34ce5d59dc06083a04b15.

--- src/main/java/com/TominoCZ/FBP/FBP.java
+++ src/main/java/com/TominoCZ/FBP/FBP.java
@@ -18,18 +18,19 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.client.IRenderHandler;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.SplittableRandom;
 
 @Mod(clientSideOnly = true, modid = ModReference.MOD_ID, name = ModReference.MOD_NAME, version = ModReference.VERSION)
 public class FBP {
@@ -57,9 +58,9 @@ public class FBP {
 	public static boolean infiniteDuration = false;
 	public static boolean randomRotation, spawnWhileFrozen, spawnRedstoneBlockParticles, randomizedScale, randomFadingSpeed, entityCollision, bounceOffWalls, lowTraction, smartBreaking, fancyPlaceAnim, spawnPlaceParticles, fancyRain, fancySnow, fancyFlame, fancySmoke, waterPhysics, restOnFloor, frozen;
 
-	public List<String> blockParticleBlacklist;
-	public List<String> blockAnimBlacklist;
-	public List<Material> floatingMaterials;
+	public static List<String> blockParticleBlacklist;
+	public static List<String> blockAnimBlacklist;
+	public static List<Material> floatingMaterials;
 
 	public static SplittableRandom random = new SplittableRandom();
 
@@ -91,21 +92,19 @@ public class FBP {
 			new Vec3d(-1, 0, 0), new Vec3d(1, 0, 0)
 	};
 
-	public static VertexFormat POSITION_TEX_COLOR_LMAP_NORMAL;
+	public static final VertexFormat POSITION_TEX_COLOR_LMAP_NORMAL = new VertexFormat();
 
-	public static FBPAnimationDummyBlock FBPBlock = new FBPAnimationDummyBlock();
+	public static final FBPAnimationDummyBlock FBPBlock = new FBPAnimationDummyBlock();
 
 	public static IRenderHandler fancyWeatherRenderer, originalWeatherRenderer;
 	public static FBPParticleManager fancyEffectRenderer;
 	public static ParticleManager originalEffectRenderer;
 
-	public FBPEventHandler eventHandler = new FBPEventHandler();
+	public static final FBPEventHandler eventHandler = new FBPEventHandler();
 	public static final FBPGuiHandler guiHandler = new FBPGuiHandler();
 
 	public FBP() {
 		INSTANCE = this;
-
-		POSITION_TEX_COLOR_LMAP_NORMAL = new VertexFormat();
 
 		POSITION_TEX_COLOR_LMAP_NORMAL.addElement(DefaultVertexFormats.POSITION_3F);
 		POSITION_TEX_COLOR_LMAP_NORMAL.addElement(DefaultVertexFormats.TEX_2F);
@@ -113,9 +112,9 @@ public class FBP {
 		POSITION_TEX_COLOR_LMAP_NORMAL.addElement(DefaultVertexFormats.TEX_2S);
 		POSITION_TEX_COLOR_LMAP_NORMAL.addElement(DefaultVertexFormats.NORMAL_3B);
 
-		blockParticleBlacklist = Collections.synchronizedList(new ArrayList<>());
-		blockAnimBlacklist = Collections.synchronizedList(new ArrayList<>());
-		floatingMaterials = Collections.synchronizedList(new ArrayList<>());
+		blockParticleBlacklist = new ArrayList<>();
+		blockAnimBlacklist = new ArrayList<>();
+		floatingMaterials = new ArrayList<>();
 	}
 
 	@EventHandler
@@ -138,11 +137,6 @@ public class FBP {
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent evt) {
 		MinecraftForge.EVENT_BUS.register(guiHandler);
-	}
-
-	@SubscribeEvent
-	public static void registerItems(final RegistryEvent.Register<Block> event) {
-		event.getRegistry().register(FBPBlock);
 	}
 
 	public static boolean isEnabled() {
