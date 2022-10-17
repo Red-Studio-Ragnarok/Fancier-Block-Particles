f53dc60d0ec6efe0d6688d6fc8fdfb66323b11cf
Desoroxxx <82710983+JustDesoroxxx@users.noreply.github.com>
2022-10-18 00:14:28 +0200

    Version Bump

--- src/main/java/com/TominoCZ/FBP/util/ModReference.java
+++ src/main/java/com/TominoCZ/FBP/util/ModReference.java
@@ -7,7 +7,7 @@ public class ModReference {
 
     public static final String MOD_ID = "fbp";
     public static final String MOD_NAME = "Fancier Block Particles";
-    public static final String VERSION = "0.6";
+    public static final String VERSION = "0.7";
     public static final URI ISSUE;
 
     static {
