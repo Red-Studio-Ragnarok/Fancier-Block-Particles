52bf8565ba0c9f0e9e74f64af550bcf7ba65b6f6
Desoroxxx <desoroxxx@gmail.com>
2023-01-08 01:50:21 +0100

    Revert `ObfuscationUtil.java`

--- src/main/java/io/redstudioragnarok/FBP/util/ObfuscationUtil.java
+++ src/main/java/io/redstudioragnarok/FBP/util/ObfuscationUtil.java
@@ -1,65 +1,41 @@
 package io.redstudioragnarok.FBP.util;
 
-import java.util.EnumMap;
-import java.util.Map;
+import java.util.HashMap;
 
 public class ObfuscationUtil {
 
-	public enum ObfMaterial {
-		field_151579_a("Air"),
-		field_151574_g("Anvil"),
-		field_175972_I("Barrier"),
-		field_151570_A("Cactus"),
-		field_151568_F("Cake"),
-		field_151593_r("Carpet"),
-		field_151594_q("Circuits"),
-		field_151571_B("Clay"),
-		field_151580_n("Cloth"),
-		field_151589_v("Coral"),
-		field_151596_z("Crafted Snow"),
-		field_151566_D("Dragon Egg"),
-		field_151581_o("Fire"),
-		field_151592_s("Glass"),
-		field_151572_C("Gourd"),
-		field_151577_b("Grass"),
-		field_151578_c("Ground"),
-		field_151588_w("Ice"),
-		field_151573_f("Iron"),
-		field_151587_i("Lava"),
-		field_151584_j("Leaves"),
-		field_151598_x("Packed Ice"),
-		field_76233_E("Piston"),
-		field_151585_k("Plants"),
-		field_151567_E("Portal"),
-		field_151591_t("Redstone Light"),
-		field_151576_e("Rock"),
-		field_151595_p("Sand"),
-		field_151597_y("Snow"),
-		field_151583_m("Sponge"),
-		field_189963_J("Structure Void"),
-		field_151590_u("Tnt"),
-		field_151582_l("Vine"),
-		field_151586_h("Water"),
-		field_151569_G("Web"),
-		field_151575_d("Wood");
+	private static final HashMap<String, String> map;
 
-		private static final Map<ObfMaterial, String> map = new EnumMap<>(ObfMaterial.class);
+	static {
+		map = new HashMap<>();
 
-		static {
-			for (ObfMaterial material : values()) {
-				map.put(material, material.clearTextName);
-			}
+		String obfNames = "field_151579_a," + "field_151577_b," + "field_151578_c," + "field_151575_d,"
+				+ "field_151576_e," + "field_151573_f," + "field_151574_g," + "field_151586_h," + "field_151587_i,"
+				+ "field_151584_j," + "field_151585_k," + "field_151582_l," + "field_151583_m," + "field_151580_n,"
+				+ "field_151581_o," + "field_151595_p," + "field_151594_q," + "field_151593_r," + "field_151592_s,"
+				+ "field_151591_t," + "field_151590_u," + "field_151589_v," + "field_151588_w," + "field_151598_x,"
+				+ "field_151597_y," + "field_151596_z," + "field_151570_A," + "field_151571_B," + "field_151572_C,"
+				+ "field_151566_D," + "field_151567_E," + "field_151568_F," + "field_151569_G," + "field_76233_E,"
+				+ "field_175972_I," + "field_189963_J";
+
+		String deobfNames = "AIR," + "GRASS," + "GROUND," + "WOOD," + "ROCK," + "IRON," + "ANVIL," + "WATER," + "LAVA,"
+				+ "LEAVES," + "PLANTS," + "VINE," + "SPONGE," + "CLOTH," + "FIRE," + "SAND," + "CIRCUITS," + "CARPET,"
+				+ "GLASS," + "REDSTONE_LIGHT," + "TNT," + "CORAL," + "ICE," + "PACKED_ICE," + "SNOW," + "CRAFTED_SNOW,"
+				+ "CACTUS," + "CLAY," + "GOURD," + "DRAGON_EGG," + "PORTAL," + "CAKE," + "WEB," + "PISTON," + "BARRIER,"
+				+ "STRUCTURE_VOID";
+
+		String[] obf = obfNames.split(",");
+		String[] deobf = deobfNames.split(",");
+
+		for (int i = 0; i < obf.length; i++) {
+			map.put(obf[i], deobf[i].toLowerCase());
 		}
+	}
 
-		private final String clearTextName;
+	public static String translateObfMaterialName(String fieldName) {
+		if (!map.containsKey(fieldName))
+			return fieldName;
 
-		ObfMaterial(String clearTextName) {
-			this.clearTextName = clearTextName;
-		}
-
-		public static String getHumanReadableName(String fieldName) {
-			ObfMaterial material = ObfMaterial.valueOf(fieldName);
-			return map.getOrDefault(material, material.clearTextName);
-		}
+		return map.get(fieldName);
 	}
 }
