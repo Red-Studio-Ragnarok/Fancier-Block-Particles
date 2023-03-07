6c8655f25fefa89a09f0ef039e4296121f42b3fe
Desoroxxx <desoroxxx@gmail.com>
2023-03-08 00:25:53 +0100

    Translation For Keybindings

--- src/main/java/io/redstudioragnarok/fbp/keys/KeyBindings.java
+++ src/main/java/io/redstudioragnarok/fbp/keys/KeyBindings.java
@@ -1,5 +1,6 @@
 package io.redstudioragnarok.fbp.keys;
 
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import org.lwjgl.input.Keyboard;
@@ -9,11 +10,11 @@ public class KeyBindings {
 	public static KeyBinding menu, freeze, toggle, killParticles, blacklistGUI;
 
 	public static void init() {
-		menu = new KeyBinding("Open Menu", Keyboard.KEY_P, "Fancier Block Particles");
-		freeze = new KeyBinding("Toggle Freeze Effect", Keyboard.KEY_NONE, "Fancier Block Particles");
-		toggle = new KeyBinding("Enable/Disable", Keyboard.KEY_NONE, "Fancier Block Particles");
-		killParticles = new KeyBinding("Kill Particles", Keyboard.KEY_NONE, "Fancier Block Particles");
-		blacklistGUI = new KeyBinding("Blacklist Block", Keyboard.KEY_B, "Fancier Block Particles");
+		menu = new KeyBinding(I18n.format("keybind.menu"), Keyboard.KEY_P, "Fancier Block Particles");
+		freeze = new KeyBinding(I18n.format("keybind.freeze"), Keyboard.KEY_NONE, "Fancier Block Particles");
+		toggle = new KeyBinding(I18n.format("keybind.toggle"), Keyboard.KEY_NONE, "Fancier Block Particles");
+		killParticles = new KeyBinding(I18n.format("keybind.kill"), Keyboard.KEY_NONE, "Fancier Block Particles");
+		blacklistGUI = new KeyBinding(I18n.format("keybind.blacklistGUI"), Keyboard.KEY_B, "Fancier Block Particles");
 
 		ClientRegistry.registerKeyBinding(menu);
 		ClientRegistry.registerKeyBinding(freeze);
