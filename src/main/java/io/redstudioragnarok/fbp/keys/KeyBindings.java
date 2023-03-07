e567acef4ffffcba7cba513d1ad497d6c8195349
Desoroxxx <desoroxxx@gmail.com>
2023-03-08 00:37:11 +0100

    Translate Keybindings Category & Reorganize Order

--- src/main/java/io/redstudioragnarok/fbp/keys/KeyBindings.java
+++ src/main/java/io/redstudioragnarok/fbp/keys/KeyBindings.java
@@ -7,18 +7,18 @@ import org.lwjgl.input.Keyboard;
 
 public class KeyBindings {
 
-	public static KeyBinding menu, freeze, toggle, killParticles, blacklistGUI;
+	public static KeyBinding menu, toggle, freeze, killParticles, blacklistGUI;
 
 	public static void init() {
-		menu = new KeyBinding(I18n.format("keybind.menu"), Keyboard.KEY_P, "Fancier Block Particles");
-		freeze = new KeyBinding(I18n.format("keybind.freeze"), Keyboard.KEY_NONE, "Fancier Block Particles");
-		toggle = new KeyBinding(I18n.format("keybind.toggle"), Keyboard.KEY_NONE, "Fancier Block Particles");
-		killParticles = new KeyBinding(I18n.format("keybind.kill"), Keyboard.KEY_NONE, "Fancier Block Particles");
-		blacklistGUI = new KeyBinding(I18n.format("keybind.blacklistGUI"), Keyboard.KEY_B, "Fancier Block Particles");
+		menu = new KeyBinding(I18n.format("keybind.menu"), Keyboard.KEY_P, I18n.format("name"));
+		toggle = new KeyBinding(I18n.format("keybind.toggle"), Keyboard.KEY_NONE, I18n.format("name"));
+		freeze = new KeyBinding(I18n.format("keybind.freeze"), Keyboard.KEY_NONE, I18n.format("name"));
+		killParticles = new KeyBinding(I18n.format("keybind.kill"), Keyboard.KEY_NONE, I18n.format("name"));
+		blacklistGUI = new KeyBinding(I18n.format("keybind.blacklistGUI"), Keyboard.KEY_B, I18n.format("name"));
 
 		ClientRegistry.registerKeyBinding(menu);
-		ClientRegistry.registerKeyBinding(freeze);
 		ClientRegistry.registerKeyBinding(toggle);
+		ClientRegistry.registerKeyBinding(freeze);
 		ClientRegistry.registerKeyBinding(killParticles);
 		ClientRegistry.registerKeyBinding(blacklistGUI);
 	}
