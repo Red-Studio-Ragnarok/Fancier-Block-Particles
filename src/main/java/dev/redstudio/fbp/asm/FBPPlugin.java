fb7fe35f3f5dffec53051c808f58098f93fd447b
Meldexun <27779321+Meldexun@users.noreply.github.com>
2024-08-01 11:12:14 +0200

    Remove code that requires mixinbooter

--- src/main/java/dev/redstudio/fbp/asm/FBPPlugin.java
+++ src/main/java/dev/redstudio/fbp/asm/FBPPlugin.java
@@ -1,15 +1,12 @@
 package dev.redstudio.fbp.asm;
 
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import zone.rong.mixinbooter.IEarlyMixinLoader;
+import org.spongepowered.asm.mixin.Mixins;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @IFMLLoadingPlugin.TransformerExclusions("io.redstudioragnarok.fbp.asm")
-public class FBPPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
+public class FBPPlugin implements IFMLLoadingPlugin {
 
 	@Override
 	public String[] getASMTransformerClass() {
@@ -28,17 +25,12 @@ public class FBPPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
 	@Override
 	public void injectData(final Map<String, Object> data) {
-
+		Mixins.addConfiguration("mixins.fbp.json");
 	}
 
 	@Override
 	public String getAccessTransformerClass() {
 		return null;
-	}
-
-	@Override
-	public List<String> getMixinConfigs() {
-		return Collections.singletonList("mixins.fbp.json");
 	}
 
 }
